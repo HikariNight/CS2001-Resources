5c5cd5b03a69af498d63049bd330b43cb264035d
HikariNight <rayjay2004.rc@gmail.com>
2024-12-07 14:49:58 +0000

    Test

--- Labs/Lab9 - Demo-Backend-App/src/main/java/com/example/demo/Controllers/UserController.java
+++ Labs/Lab9 - Demo-Backend-App/src/main/java/com/example/demo/Controllers/UserController.java
@@ -1,8 +1,10 @@
-package com.example.demo.Controllers;
+
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,14 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.DTO.UserPostDTO;
 import com.example.demo.Models.User;
+import com.example.demo.Models.UserType;
 import com.example.demo.Services.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
     @Autowired
-	UserService userService;
-    
+    UserService userService;
+
     // Get All Users
     @GetMapping("/user")
     public List<User> getUsers() {
@@ -31,40 +35,38 @@ public class UserController {
     //Post a User
     @PostMapping("/user")
     public ResponseEntity<Optional<User>> addUser(@RequestBody UserPostDTO newUserDTO) {
-    	
-    	if (newUserDTO.getName()==null || 
-    		newUserDTO.getEmail()==null ||
-    		newUserDTO.getPassword()==null ||
-    		newUserDTO.getUserType() == UserType.NONE) {
+
+        if (newUserDTO.getName() == null
+                || newUserDTO.getEmail() == null
+                || newUserDTO.getPassword() == null
+                || newUserDTO.getUserType() == UserType.NONE) {
             return new ResponseEntity<>(Optional.ofNullable(null), HttpStatus.BAD_REQUEST);
         }
-    	
-    	User newUser = new User(newUserDTO.getName(), newUserDTO.getEmail(),
-    			newUserDTO.getPassword(), newUserDTO.getUserType());
-    	userService.addUser(newUser);
-    	return new ResponseEntity<>(Optional.ofNullable(newUser),HttpStatus.CREATED);
+
+        User newUser = new User(newUserDTO.getName(), newUserDTO.getEmail(),
+                newUserDTO.getPassword(), newUserDTO.getUserType());
+        userService.addUser(newUser);
+        return new ResponseEntity<>(Optional.ofNullable(newUser), HttpStatus.CREATED);
 
     }
-    
+
     //Get User by ID
     @GetMapping("/user/{id}")
     public Optional<User> getUserById(@PathVariable(value = "id") long Id) {
         return userService.findByID(Id);
     }
-    
-    
+
     //Delete a User by ID
     @DeleteMapping("/user/{id}")
     public String deleteUser(@PathVariable(value = "id") long Id) {
         userService.deleteUser(Id);
-        return "User Deleted"; 
+        return "User Deleted";
     }
-    
+
     //Get User by Email
     @GetMapping("/user/findByEmail")
     public Optional<User> getUserByEmail(@RequestParam String email) {
-    	return Optional.ofNullable(userService.findByEmail(email));
+        return Optional.ofNullable(userService.findByEmail(email));
     }
 
-   
 }
--- Labs/Lab9 - Demo-Backend-App/src/main/java/com/example/demo/DemoApplication.java
+++ Labs/Lab9 - Demo-Backend-App/src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,3 @@
-package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,24 +6,23 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-				.allowedOrigins("http://localhost:5173");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173");
+            }
+        };
+    }
 
 }
